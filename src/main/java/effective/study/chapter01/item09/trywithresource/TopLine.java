3ab12345102e39c017c28eeb6e3791b9030efa7e
jogony <seonwongim@gmail.com>
2023-11-20 23:51:42 +0900

    item10 - equals는 일반규약을 지켜 재정의하라

--- src/main/java/effective/study/chapter01/item09/trywithresource/TopLine.java
+++ src/main/java/effective/study/chapter01/item09/trywithresource/TopLine.java
@@ -4,16 +4,16 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-    public class TopLine {
+public class TopLine {
 
-        static String firstLineOfFile(String path) throws IOException {
-            try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-                return br.readLine();
-            }
-        }
-
-        public static void main(String[] args) throws IOException {
-            String path = args[0];
-            System.out.println(firstLineOfFile(path));
+    static String firstLineOfFile(String path) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            return br.readLine();
         }
     }
+
+    public static void main(String[] args) throws IOException {
+        String path = args[0];
+        System.out.println(firstLineOfFile(path));
+    }
+}
